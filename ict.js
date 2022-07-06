c08dfe065ba8c0db6a9d7207455addb101e46d30
VandanaKopparapu <108800569+VandanaKopparapu@users.noreply.github.com>
2022-07-06 16:28:54 +0530

    Add files via upload

---
+++ ict.js
@@ -0,0 +1,679 @@
+allData = {
+  "classes": [
+    {
+      "className": 10,
+      "studentresults": [
+        {
+          "studentName": "gaurav",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "james watson",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "bumrah",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        },
+        {
+          "studentName": "swetha varma",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "david Warner",
+          "rank": 1,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "chandra sekhar",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "renuka",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "devayani",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        }
+      ]
+    },
+    {
+      "className": 9,
+      "studentresults": [
+        {
+          "studentName": "Jacky",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Michael",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Jenus warma",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        },
+        {
+          "studentName": "Oviya singh",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Richa",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Albert venin",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Wizard jeslin",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Hawai raj",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        }
+      ]
+    },
+    {
+      "className": 8,
+      "studentresults": [
+        {
+          "studentName": "Haketh khan",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Rakesh singh",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Tennis roy",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        },
+        {
+          "studentName": "Washingh sing",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Wayne Walker",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Harvey May",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Greg Titterington",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        }
+      ]
+    },
+    {
+      "className": 7,
+      "studentresults": [
+        {
+          "studentName": " Stevenson",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Abby Hardy",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Hanley ",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        },
+        {
+          "studentName": "Pamela Kennedy",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": " Pop singh",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Jeffrey",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": " Vega",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        }
+      ]
+    },
+    {
+      "className": 6,
+      "studentresults": [
+        {
+          "studentName": "Gary",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Kendrick ",
+          "rank": 2,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 368,
+          "mid2score": 369,
+          "finalscore": 401
+        },
+        {
+          "studentName": "Sarah Alikhan",
+          "rank": 3,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 234,
+          "mid2score": 326,
+          "finalscore": 289
+        },
+        {
+          "studentName": "Vergil",
+          "rank": 1,
+          "peformanceLevel": "Above-Level " + '<span class="highCircle">&#9679;</span>' + " 85%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Monica",
+          "rank": 1,
+          "peformanceLevel": "Below-Level " + '<span class="lowCircle">&#9679;</span>' + " 55%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        },
+        {
+          "studentName": "Chandana",
+          "rank": 1,
+          "peformanceLevel": "On-Level " + '<span class="medCircle">&#9679;</span>' + " 65%",
+          "mid1Score": 498,
+          "mid2score": 501,
+          "finalscore": 499
+        }
+      ]
+    }
+  ]
+}
+
+var dropDownData = {
+  "classDropdown": [
+    "Class 6",
+    "Class 7",
+    "Class 8",
+    "Class 9",
+    "Class 10"
+  ],
+  "performanceLevel": [
+    "Above-Level",
+    "On-Level",
+    "Below-Level"
+  ]
+}
+
+// drop down part
+
+
+
+let classSelect = document.getElementById('classSelector');
+for (var i = dropDownData.classDropdown.length - 1; i >= 0; i--) {
+  //console.log(dropDownData.classDropdown[i]);
+  classSelect.innerHTML += `<option value=${dropDownData.classDropdown.length - i - 1}>${dropDownData.classDropdown[i]}</option>`
+}
+
+let levelSelect = document.getElementById('levelSelector');
+levelSelect.innerHTML += `<option value=${101}>Select Level</option>`
+
+
+
+
+for (var i = 0; i < dropDownData.performanceLevel.length; i++) {
+  //console.log(dropDownData.performanceLevel[i]);
+  levelSelect.innerHTML += `<option value=${i}>${dropDownData.performanceLevel[i]}</option>`
+}
+
+let btnItem = document.getElementById('getsButton');
+
+function levelSelected() {
+  // console.log('clicked');
+  btnItem.disabled = false;
+}
+
+
+function classSelected() {
+  btnItem.disabled = !levelSelect.value;
+  btnItem.disabled = true;
+  levelSelect.selectedIndex = 0;
+}
+
+
+
+
+// table
+
+//headers
+var tablecolumns = [];
+
+
+for (var i = 0; i < allData.classes[0].studentresults.length; i++) {
+  for (var key in allData.classes[0].studentresults[i]) {
+    if (tablecolumns.indexOf(key) === -1) {
+      tablecolumns.push(key);
+    }
+  }
+}
+
+console.log(tablecolumns);
+
+var tableStudents = document.createElement("table");
+
+
+
+var trh = tableStudents.insertRow(-1);
+
+for (var i = 0; i < tablecolumns.length; i++) {
+  //header
+  var th = document.createElement("th");
+
+  th.innerHTML = tablecolumns[i];
+  if (i == 0) {
+    th.onclick = function () {
+      sortclicked();
+    }
+  }
+
+  if (i > 4) {
+    th.classList.add('hidden');
+    th.setAttribute('id', 'th5');
+  }
+  trh.appendChild(th);
+}
+
+// header inserted into the table
+
+// data is being inserted into the table
+for (var i = 0; i < allData.classes[0].studentresults.length; i++) {
+
+  tr = tableStudents.insertRow(-1);
+
+  for (var j = 0; j < tablecolumns.length; j++) {
+    var tabCell = tr.insertCell(-1);
+    if (j > 4) {
+      tabCell.classList.add('hidden');
+      tabCell.setAttribute('id', 'th' + i + j);
+    }
+
+    // tabCell.innerHTML = employess[i][tablecolumns[j]];
+    tabCell.innerHTML = allData.classes[0].studentresults[i][tablecolumns[j]];
+  }
+}
+
+
+// inserting the data
+
+
+var employeedivcontainer = document.getElementById("tableContainer");
+employeedivcontainer.innerHTML = "";
+employeedivcontainer.appendChild(tableStudents);
+
+
+
+// changing the table data
+function getData() {
+
+  btnItem.disabled = true;
+
+  var selectedClass = classSelect.value;
+  var selectedLevel = levelSelect.value;
+
+
+  let temp = dropDownData.performanceLevel[selectedLevel][0];
+
+
+  // creating a new table and replacing it with the existing one
+
+
+
+
+  var tableStudents2 = document.createElement("table");
+
+  var trh = tableStudents2.insertRow(-1);
+
+  for (var i = 0; i < tablecolumns.length; i++) {
+    //header
+    var th = document.createElement("th");
+
+    th.innerHTML = tablecolumns[i];
+    if (i == 0) {
+      th.onclick = function () {
+        sortclicked();
+      }
+    }
+    if (i > 4) {
+      th.classList.add('hidden');
+      th.setAttribute('id', 'th5');
+    }
+    trh.appendChild(th);
+  }
+
+  for (var i = 0; i < allData.classes[selectedClass].studentresults.length; i++) {
+
+
+    // console.log(allData.classes[selectedClass].studentresults[i].peformanceLevel[0]);
+
+
+
+    if (temp === allData.classes[selectedClass].studentresults[i].peformanceLevel[0]) {
+
+
+      // tableStudents2.deleteRow(1);
+      tr = tableStudents2.insertRow(-1);
+
+      for (var j = 0; j < tablecolumns.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        if (j > 4) {
+          tabCell.classList.add('hidden');
+          tabCell.setAttribute('id', 'th' + i + j);
+        }
+
+        // tabCell.innerHTML = employess[i][tablecolumns[j]];
+        tabCell.innerHTML = allData.classes[selectedClass].studentresults[i][tablecolumns[j]];
+      }
+    }
+  }
+
+  var employeedivcontainer = document.getElementById("tableContainer");
+  employeedivcontainer.innerHTML = "";
+  employeedivcontainer.appendChild(tableStudents2);
+
+}
+
+function dynamicsort(property, order) {
+  var sort_order = 1;
+  if (order === "desc") {
+    sort_order = -1;
+  }
+  return function (a, b) {
+    // a should come before b in the sorted order
+    if (a[property] < b[property]) {
+      return -1 * sort_order;
+      // a should come after b in the sorted order
+    } else if (a[property] > b[property]) {
+      return 1 * sort_order;
+      // a and b are the same
+    } else {
+      return 0 * sort_order;
+    }
+  }
+}
+var order = true;
+function sortclicked() {
+  let tempOrder;
+  if (order) {
+    tempOrder = 'asc';
+  } else {
+    tempOrder = 'desc';
+  }
+  var selectedClass = classSelect.value;
+
+  var data = allData.classes[selectedClass].studentresults.sort(dynamicsort('studentName', tempOrder));
+  //var data = tempOrder;
+  buildSortedTable(data);
+  order = !order;
+}
+
+function buildSortedTable(data) {
+
+
+  var selectedLevel = levelSelect.value;
+
+  var tableStudents3 = document.createElement("table");
+
+  var trh = tableStudents3.insertRow(-1);
+
+  for (var i = 0; i < tablecolumns.length; i++) {
+    //header
+    var th = document.createElement("th");
+
+    th.innerHTML = tablecolumns[i];
+    if (i == 0) {
+      th.onclick = function () {
+        sortclicked();
+      }
+    }
+    if (i > 4) {
+      th.classList.add('hidden');
+      th.setAttribute('id', 'th5');
+    }
+    trh.appendChild(th);
+  }
+
+  if (selectedLevel > 50) {
+    for (var i = 0; i < data.length; i++) {
+
+      tr = tableStudents3.insertRow(-1);
+
+      for (var j = 0; j < tablecolumns.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        if (j > 4) {
+          tabCell.classList.add('hidden');
+          tabCell.setAttribute('id', 'th' + i + j);
+        }
+
+        tabCell.innerHTML = data[i][tablecolumns[j]];
+      }
+
+    }
+    var employeedivcontainer = document.getElementById("tableContainer");
+    employeedivcontainer.innerHTML = "";
+    employeedivcontainer.appendChild(tableStudents3);
+
+
+    return;
+  }
+
+  let temp = dropDownData.performanceLevel[selectedLevel][0];
+
+  for (var i = 0; i < data.length; i++) {
+    if (temp === data[i].peformanceLevel[0]) {
+
+      tr = tableStudents3.insertRow(-1);
+
+      for (var j = 0; j < tablecolumns.length; j++) {
+        var tabCell = tr.insertCell(-1);
+        if (j > 4) {
+          tabCell.classList.add('hidden');
+          tabCell.setAttribute('id', 'th' + i + j);
+        }
+
+        tabCell.innerHTML = data[i][tablecolumns[j]];
+      }
+    }
+  }
+
+  var employeedivcontainer = document.getElementById("tableContainer");
+  employeedivcontainer.innerHTML = "";
+  employeedivcontainer.appendChild(tableStudents3);
+
+}
+
+
+
+function Scroller(ele) {
+  if (ele.value == 0) {
+
+    //header
+    for (var i = 5; i < tablecolumns.length; i++) {
+      //header
+      var th = document.getElementById("th5");
+
+      th.classList.add('hidden');
+
+    }
+
+    //body
+
+    for (var i = 0; i < allData.classes[0].studentresults.length; i++) {
+      for (var j = 0; j < tablecolumns.length; j++) {
+        try {
+          var tabCell = document.getElementById('th' + i + j);
+          if (j > 4) {
+            tabCell.classList.add('hidden');
+          }
+        } catch (error) {
+
+          var counter = 0;
+        }
+
+      }
+    }
+
+
+
+
+  } else {
+    for (var i = 5; i < tablecolumns.length; i++) {
+      //header
+      var th = document.getElementById("th5");
+      th.classList.remove('hidden');
+
+    }
+
+
+    for (var i = 0; i < allData.classes[0].studentresults.length; i++) {
+      for (var j = 0; j < tablecolumns.length; j++) {
+
+        try {
+          var tabCell = document.getElementById('th' + i + j);
+          if (j > 4) {
+            tabCell.classList.remove('hidden');
+          }
+        } catch (error) {
+          var some = 0;
+        }
+
+      }
+    }
+
+  }
+}
